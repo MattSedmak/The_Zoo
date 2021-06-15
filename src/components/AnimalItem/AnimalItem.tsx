47c8a284516f644ed9b1a91efdc4bff72f585821
MattSedmak <sedmakmatt@gmail.com>
2021-06-15 17:19:16 +0200

    Added state & 4hr timer

--- src/components/AnimalItem/AnimalItem.tsx
+++ src/components/AnimalItem/AnimalItem.tsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { IAnimal } from '../../models/Animal';
+import { IAnimalDetail } from '../../models/AnimalDetail';
 import FeedBadge from '../UI/FeedBadge';
 
-const AnimalItem = (props: IAnimal) => {
+interface itemProps {
+  isHungry: boolean;
+  animal: IAnimalDetail;
+}
+
+const AnimalItem = (props: itemProps) => {
+  const [pleaseFeed, setPleaseFeed] = useState(false);
+
+  useEffect(() => {
+    let diff = new Date().getTime() - new Date(props.animal.lastFed).getTime();
+    let diffHours = Math.floor(diff / 1000);
+    if (diffHours >= 12) {
+      setPleaseFeed(true);
+    }
+  }, [pleaseFeed]);
+
   return (
     <div>
-      <h3>{props.name}</h3>
-      <p>{props.shortDescription}</p>
-      <FeedBadge />
-      <Link to={`/animal/${props.id}`}>read more</Link>
+      <h3>{props.animal.name}</h3>
+      <p>{props.animal.shortDescription}</p>
+      {pleaseFeed && <FeedBadge />}
+
+      <Link to={`/animal/${props.animal.id}`}>read more</Link>
     </div>
   );
 };
