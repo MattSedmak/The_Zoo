cbcc7a116a2b8595d4fed486f1ad9de00e9ab0b8
MattSedmak <sedmakmatt@gmail.com>
2021-06-15 17:17:17 +0200

    Lifted state & h1 styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Animals from './components/Animals/Animals';
-
+import axios from 'axios';
 import { AnimailDetail } from './components/AnimalDetail/AnimalDetail';
+import { IAnimalDetail } from './models/AnimalDetail';
+import PageNotFound from './components/UI/PageNotFound';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-size: 3rem;
+  text-align: center;
+`;
 
 function App() {
+  const [animals, setAnimals] = useState<IAnimalDetail[]>([]);
+  const [Error, setError] = useState(false);
+  const [needsFeeding, setNeedsFeeding] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('animals') === null) {
+      axios
+        .get<IAnimalDetail[]>('https://animals.azurewebsites.net/api/animals')
+        .then((response) => {
+          setAnimals(response.data);
+          localStorage.setItem('animals', JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          setError(true);
+        });
+    } else {
+      let storedAnimals = JSON.parse(localStorage.getItem('animals') || '{}');
+      setAnimals(storedAnimals);
+    }
+  }, [needsFeeding]);
+
+  const feedAnimalHandler = (isFed: boolean) => {
+    setNeedsFeeding(isFed);
+  };
+
   return (
     <Router>
-      <h1>The Zoo</h1>
+      <Heading>The Zoo</Heading>
       <Switch>
         <Route exact path='/'>
-          <Animals />
+          <Animals animals={animals} error={Error} feed={needsFeeding} />
         </Route>
         <Route path='/animal/:id'>
-          <AnimailDetail />
+          <AnimailDetail onFeedAnimal={feedAnimalHandler} feed={needsFeeding} />
+        </Route>
+        <Route path='*'>
+          <PageNotFound />
         </Route>
       </Switch>
     </Router>
