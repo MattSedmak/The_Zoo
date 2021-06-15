e9720f6834a10393d62b18814b9c9129407f04af
MattSedmak <sedmakmatt@gmail.com>
2021-06-15 17:20:41 +0200

    refactoring

--- src/components/Animals/Animals.tsx
+++ src/components/Animals/Animals.tsx
@@ -1,45 +1,22 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import AnimalItem from '../AnimalItem/AnimalItem';
-import { IAnimal } from '../../models/Animal';
 import ErrorMsg from '../UI/ErrorMsg';
+import { IAnimalDetail } from '../../models/AnimalDetail';
 
-const Animals = () => {
-  const [animals, setAnimals] = useState<IAnimal[]>([]);
-  const [isError, setIsError] = useState(false);
+interface animalProps {
+  animals: IAnimalDetail[];
+  error: boolean;
+  feed: boolean;
+}
 
-  useEffect(() => {
-    if (localStorage.getItem('animals') === null) {
-      axios
-        .get<IAnimal[]>('https://animals.azurewebsites.net/api/animals')
-        .then((response) => {
-          setAnimals(response.data);
-          localStorage.setItem('animals', JSON.stringify(response.data));
-        })
-        .catch((error) => {
-          setIsError(true);
-        });
-    } else {
-      let storedAnimals = JSON.parse(localStorage.getItem('animals') || '{}');
-      setAnimals(storedAnimals);
-    }
-  }, []);
-
-  let zooAnimals = animals.map((animal) => {
-    return (
-      <AnimalItem
-        key={animal.id}
-        id={animal.id}
-        name={animal.name}
-        shortDescription={animal.shortDescription}
-      />
-    );
+const Animals = (props: animalProps) => {
+  let zooAnimals = props.animals.map((animal) => {
+    return <AnimalItem key={animal.id} animal={animal} isHungry={props.feed} />;
   });
 
   return (
     <div>
       <h3>Here are the animals</h3>
-      {isError && <ErrorMsg />}
+      {props.error && <ErrorMsg />}
       {zooAnimals}
     </div>
   );
